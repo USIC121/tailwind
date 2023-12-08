ad62ffbeacb626959076e707cf65e6e5af04db90
unknown <samigirum94@gmail.com>
2023-12-08 09:36:19 -0800

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*html'],
+  theme: {
+    screens:{
+sm:'480px',
+md:'786px',
+lg:'976px',
+xl:'1440px'
+    },
+    extend: {
+
+    }
+ 
+  },
+  plugins: [],
+}
+
